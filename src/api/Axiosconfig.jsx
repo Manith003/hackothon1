de6dd131e9862d3f4c15faa2e6229566b9ac7cf9
manith003 <manithkumar8190@gmail.com>
2025-07-25 20:41:32 +0530

    axios updated

--- src/api/Axiosconfig.jsx
+++ src/api/Axiosconfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://hackothon1.netlify.app/'
+    baseURL: 'https://hackothon1-production.up.railway.app/'
 });
 
 export default instance;
